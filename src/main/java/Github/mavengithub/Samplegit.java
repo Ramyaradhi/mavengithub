d7a701cd2da1f5837ddeda37481c7c26ef309f06
srira <srira@LAPTOP-NISBTSBK>
2021-08-10 18:18:30 +0100

    first commit

--- src/main/java/Github/mavengithub/Samplegit.java
+++ src/main/java/Github/mavengithub/Samplegit.java
@@ -4,7 +4,8 @@ public class Samplegit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+ 
+		 System.out.println("This is sample project");
 	}
 
 }
--- src/main/java/Github/mavengithub/verifylinks.java
+++ src/main/java/Github/mavengithub/verifylinks.java
@@ -4,7 +4,8 @@ public class verifylinks {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+     System.out.println("The new line added");
+     System.out.println("extra line added");
 	}
 
 }
